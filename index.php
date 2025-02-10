3a8cb223b668acf0b417d82ff6d1c0b6a2003fbc
Saman Saeed <samansaeed2306@gmail.com>
2025-02-10 16:27:43 +0500

    Delete last task from tasks

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 $db = new PDO('sqlite:database.sqlite');
 
+//Remove last task
+$db->exec("DELETE FROM tasks WHERE id = (SELECT MAX(id) FROM tasks)");
+echo "Last row deleted!";
+
 //For inserting data into sql tables, we do exec
-$db -> exec("INSERT INTO tasks (task) VALUES ('INSERTED WITH PHP')");
-echo "INSERTED!";
+// $db -> exec("INSERT INTO tasks (task) VALUES ('INSERTED WITH PHP')");
+// echo "INSERTED!";
 
 
 // For pulling data from sql, we do 'query'
