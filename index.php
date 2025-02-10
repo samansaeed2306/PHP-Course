6a642b7945d092662a1c44b1b44d423ddea1ed4d
Saman Saeed <samansaeed2306@gmail.com>
2025-02-10 16:09:48 +0500

    Insert query with exec()

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 $db = new PDO('sqlite:database.sqlite');
-$result = $db->query("SELECT * from tasks");
 
-foreach($result as $row){
-    echo $row['id'] .  ":" . $row['task'] ." <br>";
-}
+//For inserting data into sql tables, we do exec
+$db -> exec("INSERT INTO tasks (task) VALUES ('INSERTED WITH PHP')");
+echo "INSERTED!";
+
+
+// For pulling data from sql, we do 'query'
+// $result = $db->query("SELECT * from tasks");
+
+// foreach($result as $row){
+//     echo $row['id'] .  ":" . $row['task'] ." <br>";
+// }
