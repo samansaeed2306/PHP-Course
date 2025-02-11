c24f674c2d85dbf65815c1cc272f4846c0f86cd4
Saman Saeed <samansaeed2306@gmail.com>
2025-02-11 15:02:18 +0500

    Add constants in two ways

---
+++ consts.php
@@ -0,0 +1,14 @@
+<?php
+/* We can initilize constants in php in two ways:
+ 1. using define keyword
+ 2. using const keyword 
+*/
+
+// Define keyword
+// define("secretCode","2132");
+
+
+// Using const keyword
+const secretCode = 1232;
+
+echo secretCode;
