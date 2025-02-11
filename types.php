5d96ca6302fc25798171fc2eb49c019602067efc
Saman Saeed <samansaeed2306@gmail.com>
2025-02-11 13:51:34 +0500

    Introduce data types in php

---
+++ types.php
@@ -0,0 +1,11 @@
+<?php
+/* We have multiple data types in php including float, int, string, arrays, boolean */
+
+var_dump(5); //int(5)
+var_dump(6.5); //float(6.5)
+var_dump("45"); //string(2)
+var_dump('hello'); //string(5)
+var_dump([2,"Sam",1.5,true]); //array(4)
+var_dump(false); //bool(false)
+var_dump(NULL); //NULL
+var_dump(null); //NULL
