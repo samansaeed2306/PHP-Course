f288310806083957306cd2209737a0ddad479039
Saman Saeed <samansaeed2306@gmail.com>
2025-02-11 16:05:00 +0500

    Learn about magic constants

--- consts.php
+++ consts.php
@@ -2,6 +2,7 @@
 /* We can initilize constants in php in two ways:
  1. using define keyword
  2. using const keyword 
+ We also have MAGIC CONSTANTS IN PHP LIKE __FILE__ OR __DIR__
 */
 
 // Define keyword
@@ -12,3 +13,16 @@
 const secretCode = 1232;
 
 echo secretCode;
+
+
+//MAGIC CONSTS LIKE __FILE__ TELL US IN WHICH FILE WE ARE RIGHT NOW
+ echo __FILE__;
+ echo "<br>";
+ echo __DIR__;
+ echo "<br>";
+ function helloWorld(){
+ echo __FUNCTION__;
+ }
+ helloWorld();
+ 
+
