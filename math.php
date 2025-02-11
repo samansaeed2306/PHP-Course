043bd186690ba7de8eab24751631c8cd14c0fe08
Saman Saeed <samansaeed2306@gmail.com>
2025-02-11 14:32:47 +0500

    Basic mathematical ops in php

---
+++ math.php
@@ -0,0 +1,14 @@
+<?php
+//Math in php follows the BODMAS rule.
+
+echo 2 + 2;
+echo "<br>";
+echo 2 / 2;
+echo "<br>";
+echo 2 - 2;
+echo "<br>";
+echo 2 * 2;
+echo "<br>";
+echo 2 % 2;
+echo "<br>";
+echo 4 ** 2;
