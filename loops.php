3b18e1c543726125e910be8291a4c5efcf379d9d
Saman Saeed <samansaeed2306@gmail.com>
2025-02-12 14:08:49 +0500

    Add breaks and continue statements

--- loops.php
+++ loops.php
@@ -1,6 +1,18 @@
 <?php
 $person = ["name"=>"Saman", "age" => "22"];
 
-foreach($persons as $key => $value){
+foreach($person as $key => $value){
     echo "$key: $value <br>";
+}
+
+$colors = ["purple","white","red","yellow"];
+foreach($colors as $color){
+
+if($color=="red"){
+    //continue to skip it, break to come out of the loop 
+    // break;
+    continue;
+}
+echo "$color <br>";
+
 }
