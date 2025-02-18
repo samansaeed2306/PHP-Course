305bf02e68a354577a48e9cd9714ee491e704658
Saman Saeed <samansaeed2306@gmail.com>
2025-02-18 12:23:34 +0500

    Learn about math functions in php

---
+++ mathFunctions.php
@@ -0,0 +1,52 @@
+<?php
+//Absolute value of a number 
+echo abs(-10);
+
+echo "<br>";
+//Round off numbers
+echo round(3.68);
+
+echo "<br>";
+
+//Ceiling of numbers
+echo ceil(4.28);
+
+echo "<br>";
+//Floor of num
+echo floor(3.8);
+
+echo "<br>";
+
+$numbers=[10,20,50,100];
+
+//max of nums
+echo max($numbers);
+
+echo "<br>";
+
+//min of nums
+echo min($numbers);
+
+$num = 64;
+echo "<br>";
+
+//Sqroot of a num 
+echo sqrt($num);
+
+echo "<br>";
+
+$base = 2;
+$exp = 4;
+//base to the Power exponent 
+echo pow($base,$exp);
+
+echo "<br>";
+
+//random number between two numbers
+echo rand(200,400);
+
+
+
+
+
+
