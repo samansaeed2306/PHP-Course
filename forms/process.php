dec7d33bbe3b1d2a55d9aeaca4aaf393f7194038
Saman Saeed <samansaeed2306@gmail.com>
2025-02-18 16:56:47 +0500

    Add trim() and htmlspecialchars() to avoid js run in fields

--- forms/process.php
+++ forms/process.php
@@ -2,9 +2,9 @@
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     ECHO "POST REQ";
     echo "<br>";
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $status = $_POST["status"];
+    $username = htmlspecialchars(trim($_POST["username"]));
+    $password =htmlspecialchars(trim( $_POST["password"]));
+    $status = htmlspecialchars(trim($_POST["status"]));
 
     echo $username . "<br>";
     echo $password . "<br>";
